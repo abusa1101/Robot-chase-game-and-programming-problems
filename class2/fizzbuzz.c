cda241bd1dfba4a3297e4534f10aec264675a99d
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-04 15:10:10 -0400

    fizzbuzz basic code files

--- class2/fizzbuzz.c
+++ class2/fizzbuzz.c
@@ -2,18 +2,18 @@
 
 int main(void) {
     int i = 0;
-    while (i<100){
-        if (i%15==0){
+    while (i<100) {
+        if (i % 15 == 0) {
             printf("fizzbuzz\n");
         }
-        if (i%5==0){
+        if (i % 5 == 0) {
             printf("fizz\n");
         }
-        else if (i%3==0){
+        else if (i % 3 == 0) {
             printf("buzz\n");
         }
-        else{
-            printf("%d\n",i);
+        else {
+            printf("%d\n", i);
         }
         i++;
     }
