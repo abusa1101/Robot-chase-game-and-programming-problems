31ec76a1d5d273da87a45f68fb85607a1cc17a49
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-10 16:18:51 -0400

    upper files

--- class3/upper.c
+++ class3/upper.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
-int main(void) {
-    //printf("Enter text to capitalize:\n"); //read user input
-    //char word;
-    //int vals_read = scanf("%s", word); //or whatever input
-    char word = "hello";
-    int len = strlen(word);
+int main(int argc, char **argv) {
 
-    for (int i = o; i <= len; i++){
-        if (word(i) >= 97 && word(i) <= 122){
-            word(i)= word(i) - 32;
-            letter = word(i);
-            printf("%s", letter);
+    char letter;
+    letter = argv[1][0];
+    int k = 0;
+    //int len2 = strlen(argv[2]);
+    for (int j = 1; j < argc; j++){ //j is which word are you on
+        int len = strlen(argv[j]);
+        for (int i = 0; i < len; i++){ //i is which letter of the word you are on
+            letter = argv[j][i];
+            if (letter >= 96 & letter <= 122){
+                letter = (letter - 32);
+                printf("%c", letter);
+            }
+
+            else if (letter == 34){
+                k = 1;
+                letter = argv[j][i+1];
+                if (k == 1){
+                    printf("\n");
+                }
+            
+            }
+
+            else {
+                printf("%c", letter);
+                
+            }
         }
-        else {
-            printf("Enter lowercase letters between 'a' and 'z' only.\n");
-            return 1;
-        }
+        k = 0;
     }
-
-
-
-    /*if (vals_read = ) {
-        fprintf(stderr, "Invalid input. Please enter a word.\n");
-        return 1;
-    }*/
-    
+    printf("\n"); 
 }
+
 
