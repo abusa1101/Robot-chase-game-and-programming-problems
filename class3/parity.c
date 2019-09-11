0ec831c5593fdf0a852d524746d8feb92764b590
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-11 15:32:26 -0400

    parity files

--- class3/parity.c
+++ class3/parity.c
@@ -6,22 +6,25 @@ unsigned char text = argv[1][0];
 if (argc == 1) { // Print 0 if no input is given
     printf("0");
 }
+
 if (argc > 2) { // Print error message if too many arguments
     printf("Too many arguments\n");
     return 1;
 }
+
 int len = strlen(argv[1]);
 int counter = 0;
 int totalcount = 0;
 for (int i = 0; i < len; i++) { //i is which letter of the word you are on
     text = argv[1][i];
     for (counter = 0; text != 0; text >>= 1) { //for each letter
-        if(text & 01) {
+        if (text & 01) {
             counter++; //counts the number of ones in the word's binary form
-            totalcount = totalcount + 1;           
+            totalcount = totalcount + 1;          
         }
     }
 }
+
 if (totalcount % 2 == 0) {
     printf("0\n");
 } else {
