4956e421771b8dc09825c7e3f69b826b2d0930cd
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-13 11:42:20 -0400

    tricolor files

--- class3/tricolor.c
+++ class3/tricolor.c
@@ -17,7 +17,7 @@ int main(void) {
         trit_encodings[0][2] = ' ';
         trit_encodings[0][3] = ' ';
         trit_encodings[0][4] = ' ';
-    }//printf("%c\n",trit_encodings[0][0]); //printf("%d",trinum[0]); 
+    } //printf("%c\n",trit_encodings[0][0]); //printf("%d",trinum[0]);
 
     for (i = 1; i < 243; i++) {
         trit_encodings[i] = calloc(sizeof(char), 6);
@@ -29,7 +29,7 @@ int main(void) {
         trit_encodings[i][4] = ' ';
         while (val > 0) {
             trinum[i] = val % 3;
-            val = val / 3;//printf("%d",trinum[i]);
+            val = val / 3; //printf("%d",trinum[i]);
             if (trinum[i] == 0) {
                 tri[i] = ' ';
             }
@@ -38,13 +38,13 @@ int main(void) {
             }
             if (trinum[i] == 2) {
                 tri[i] = '@';
-            }//printf("%c",tri[i]);
+            } //printf("%c",tri[i]);
             trit_encodings[i][j] = tri[i];
             j = j + 1;
-        }//printf("\n");These print statements were placed on the next line
+        } //printf("\n");These print statements were placed on the next line
     }
 
-    FILE *f; // PART 2: IMAGE MANIPULATION
+    FILE *f; //PART 2: IMAGE MANIPULATION
     f = fopen("img.bin", "rb");
     for (int row = 0; row < 40; row++) {
         for (int byte = 0; byte < 18; byte++)  {
