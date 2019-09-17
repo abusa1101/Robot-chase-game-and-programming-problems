43d08b890223ef23afb67ad2148bf0b03156bcdb
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-17 15:16:05 -0400

    addresses files

--- class4/addresses.c
+++ class4/addresses.c
@@ -12,26 +12,26 @@ int main(void) {
 
     printf("%p\n", (void *)&int1); 
     printf("%p\n", (void *)&char1); 
-    printf("%ld\n", (long)&int1 - (long)&char1);
+    printf("%p\n", (long)&int1 - (long)&char1);
     printf("%p\n", (void *)&doubles); 
     printf("%p\n", (void *)&doubles[0]); 
     printf("%p\n", (void *)&doubles[1]); 
     printf("%p\n", (void *)&doubles[2]);
-    printf("%ld\n", (long)&doubles[0] - (long)&doubles[1]);
-    printf("%ld\n", (long)&doubles[1] - (long)&doubles[2]);
+    printf("%p\n", (long)&doubles[0] - (long)&doubles[1]);
+    printf("%p\n", (long)&doubles[1] - (long)&doubles[2]);
     printf("%p\n", (void *)&int2); 
     printf("%p\n", (void *)&char2); 
     printf("%p\n", (void *)&floats); 
     printf("%p\n", (void *)&floats[0]); 
     printf("%p\n", (void *)&floats[1]); 
     printf("%p\n", (void *)&floats[2]);
-    printf("%ld\n", (long)&floats[0] - (long)&floats[1]);
-    printf("%ld\n", (long)&floats[1] - (long)&floats[2]);
+    printf("%p\n", (long)&floats[0] - (long)&floats[1]);
+    printf("%p\n", (long)&floats[1] - (long)&floats[2]);
 
     void *mem = malloc(1);  // ask for memory
     printf("%p\n", mem);    // memory is already "given" to us as an address!
     free(mem);              // give memory back
-    
+
     return 0;
 
 }
