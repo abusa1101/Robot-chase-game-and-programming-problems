846032fed377fefc917fe6644ae712c0721cd078
Anviksha R Busa <abusa@beyster1695p05.engin.umich.edu>
2019-09-16 21:37:08 -0400

    collision files

--- homework1/collision.c
+++ homework1/collision.c
@@ -7,7 +7,7 @@ int main(void) {
     char c[100];
     double d[200]; //all data array
 
-    //PART 1(a): Read the csv file 
+    //PART 1: Read the csv file 
     FILE *f = fopen("polygons.csv", "r");
 
     if (!f){
@@ -35,12 +35,12 @@ int main(void) {
         values++;
     }
 
-    //PART 1(b): Store read data. d[i] = first data point, d[values-1] = last data point
+    //PART 2: Store read data. d[i] = first data point, d[values-1] = last data point
     int n = d[m+3];
     double xpivot = 0; //constant
     double ypivot = 0; //constant
 
-    //Part 2: Read n points and display error msg if n > 16
+    //Read n points and display error msg if n > 16
     if (n > 16){
         fprintf(stderr, "Error: Too many points (n < 16)\n");
         return 1;
@@ -92,37 +92,31 @@ int main(void) {
     double p2_transformed_y[n];
 
     for (int i = 1; i <= n; i++){ 
-        //Polygon 1: Global rotation
+        //Polygon 1: Global transformation
         double p1_xshifted = p1_x[i] - xpivot; 
         double p1_yshifted = p1_y[i] - ypivot; 
         double p1_xrotated = xpivot + (p1_xshifted*cos(p1_radrot) - p1_yshifted*sin(p1_radrot)); 
         double p1_yrotated = ypivot + (p1_xshifted*sin(p1_radrot) + p1_yshifted*cos(p1_radrot));
-        //Polygon 1: Global translation
         double p1_xtrans = p1_xrotated + p1_xglobal; 
         double p1_ytrans = p1_yrotated + p1_yglobal;
         p1_transformed_x[i] = p1_xtrans;
         p1_transformed_y[i] = p1_ytrans;
 
-        //Polygon 2: Global rotation
+        //Polygon 2: Global transformation
         double p2_xshifted = p2_x[i] - xpivot; 
         double p2_yshifted = p2_y[i] - ypivot; 
         double p2_xrotated = xpivot + (p2_xshifted*cos(p2_radrot) - p2_yshifted*sin(p2_radrot)); 
         double p2_yrotated = ypivot + (p2_xshifted*sin(p2_radrot) + p2_yshifted*cos(p2_radrot));
-        //Polygon 2: Global translation
         double p2_xtrans = p2_xrotated + p2_xglobal; 
         double p2_ytrans = p2_yrotated + p2_yglobal;
         p2_transformed_x[i] = p2_xtrans;
         p2_transformed_y[i] = p2_ytrans;
     }
 
-    //Part 4: Make an array of lines, slopes, y-intercepts for both polygons
-
-    //Part 5: Collision algorithm!
-    int collision = 0; //collision condition
-    int l1_opp = 0; //cp condition: 0 = same side, 1 = opp side, 2 = zero vector
+    //Part 4: Collision algorithm!
+    int collision = 0;  //collision condition
+    int l1_opp = 0;     //cp condition: 0 = same side, 1 = opp side, 2 = zero vector
     int l2_opp = 0;
-    double slope_p1[n];
-    double slope_p2[n];
 
     for (int u = 1; u <= n; u++ ) {
         if (u == n){
@@ -148,8 +142,7 @@ int main(void) {
             double cp2_2 = (line2_x * p1_transformed_y[u+1]) - (line2_y * p1_transformed_x[u+1]);
             //check for likeness or signs of crossproduct
             //line 1 and line 2 points
-            if (cp1_1 > 0 & cp1_2 < 0) { //if the two cross products have opposite sign, or one of them is zero
-                //then they were on opposite sides of/touching the line l that extends to infinity
+            if (cp1_1 > 0 & cp1_2 < 0) { 
                 l1_opp = 1;
             } 
             else if (cp1_1 < 0 & cp1_2 > 0) {
@@ -161,7 +154,7 @@ int main(void) {
                 l1_opp  = 0;
             }
             //line 2 and line 1 points
-            if (cp2_1 > 0 & cp2_2 < 0) { //if the two cross products have opposite sign, or one of them is zero
+            if (cp2_1 > 0 & cp2_2 < 0) { 
                 l2_opp = 1;
             } 
             else if (cp2_1 < 0 & cp2_2 > 0) {
@@ -175,21 +168,16 @@ int main(void) {
 
             //check for collision!
             if (l1_opp == 1 && l2_opp == 1) { //if both cross product checks above indicated opposite signs,
-                    //printf("Actual lines intersect!!\n"); //then the actual lines (not extending to infinity) intersect,
                     collision = 1;
             } else if (l1_opp == 2 || l2_opp == 2) { //EXCEPT when both checks (for lines l1 and l2) have at least one zero cross product
-                    //printf("There is atleast one zero cross product");
                     collision = 1;
             } else if (l1_opp == 2 && l2_opp == 2){
-                    //printf("There is atleast one zero cross product");
                     collision = 0;
             } else {
-                //printf("Actual lines DO NOT intersect\n");
                 collision = 0;
             }
         }
     }
-
 
     //Part 6: Check Failure Mode! i.e. check if either polygon contains the other
     //POLYGON 1: CONTAINMENT
@@ -235,10 +223,8 @@ int main(void) {
         }
         double line2_x = p2_transformed_x[v+1] - p2_transformed_x[v];
         double line2_y = p2_transformed_y[v+1] - p2_transformed_y[v];
-
         //for one p in the other line, compute the cross product between l and p
         double cp2_1 = (line2_x * p1_transformed_y[v]) - (line2_y * p1_transformed_x[v]);
-        //check for likeness or signs of crossproduct
         //check for likeness or signs of crossproduct
         if (cp2_1 > 0) {
             p2_sign[v] = 1;
@@ -261,9 +247,7 @@ int main(void) {
 
     //Part 7: Declare collision or not!! 
     if (collision == 1) {
-        //if(contained_collision != 0){
-            printf("collision!\n");
-        //}
+        printf("collision!\n");
     } else {
         printf("no collision\n");
     }
