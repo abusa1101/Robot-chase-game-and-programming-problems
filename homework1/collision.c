fd1e04bfb438643455ee4b882d95fb642880753f
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-18 02:09:10 -0400

    coll files

--- homework1/collision.c
+++ homework1/collision.c
@@ -173,6 +173,8 @@ int main(void) {
             //check for collision!
             if (l1_opp == 1 && l2_opp == 1) { //if both cross product checks above indicated opposite signs,
                     collision = 1;
+            } else if (l1_opp == 1 || l2_opp == 1) { //EXCEPT when both checks (for lines l1 and l2) have at least one zero cross product
+                    collision = 1;
             } else if (l1_opp == 2 || l2_opp == 2) { //EXCEPT when both checks (for lines l1 and l2) have at least one zero cross product
                     collision = 1;
             } else if (l1_opp == 2 && l2_opp == 2){
