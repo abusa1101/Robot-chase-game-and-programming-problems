7bb0244f9f7b88d349ccb80b18c1cf461d2efc2e
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-09-18 13:29:32 -0400

    coll files

--- homework1/collision.c
+++ homework1/collision.c
@@ -247,7 +247,7 @@ int main(void) {
     //Part 7: Declare collision or not!!
     if (collision == 1 && contained_collision == 1) {
         printf("collision!\n");
-    } else if (collision == 0 || contained_collision == 0) {
+    } else if (collision == 0 && contained_collision == 0) {
         printf("collision!\n");
     } else {
         printf("no collision\n");
