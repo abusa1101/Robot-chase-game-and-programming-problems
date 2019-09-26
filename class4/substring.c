6464b78ad7c2e775f1904e341bc9b26506f8b587
abusa <abusa@umich.edu>
2019-09-25 20:12:27 -0400

    Replace substring.c

--- class4/substring.c
+++ class4/substring.c
@@ -1,58 +1,69 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 char file_buffer[64 * 1024];
 
-int get_context (int key_ptr){
-  //get the starting location
-  if (argv[3][0] == '0'){
-    printf("%s", argv[2]);
-  } else if (argv[3][0] == '1'){
-    //do something
-  }
+char *print_context(char *p, int count, int key_len) {
+    if (count == 0) {
+        int m = 0;
+        while (m < key_len) {
+            printf("%c", p[m]);
+            m++;
+        }
+        printf("\n");
+        printf("\n");
+        return &p[m];
+    }
+    int i = 0;
+    for (int n = 0; n < count; n++) {
+        while (p[-i] != '\n' && &p[-i] >= file_buffer) {
+            i++;
+        }
+        if (&p[-i] >= file_buffer) {
+            i++;
+        }
+    }
+    int j;
+    if (&p[-i] <= file_buffer) {
+        j = -i + 1;
+    } else {
+        j = -i + 2;
+    }
+    while (j < key_len) {
+        printf("%c", p[j]);
+        j++;
+    }
+    printf("\n");
+    printf("\n");
+    return &p[j];
 }
 
 int main(int argc, char **argv) {
-  //Setup and Failure/Error code////////////////////////////////////////////////
     if (argc != 4) {
         fprintf(stderr, "usage: %s <file> <key> <lines before>\n", argv[0]);
         return 1;
     }
     FILE *f = fopen(argv[1], "r");
+    int text = atoi(argv[3]);
+    int key_len = strlen(argv[2]);
     if (!f) {
         fprintf(stderr, "Could not open %s: ", argv[1]);
         perror("");
         return 1;
     }
-    int bytes_read = fread(file_buffer, 1, sizeof(file_buffer) - 1, f);
+    fread(file_buffer, 1, sizeof(file_buffer) - 1, f);
     if (!feof(f)) {
         fprintf(stderr, "Could not read entire file. Is it too big?\n");
         return 1;
     }
+    char *p = strstr(file_buffer, argv[2]);
+    while (p) {
+        char *p = print_context(p, text, key_len);
+        p = strstr(p, argv[2]);
+    }
     fclose(f);
-    // we want this to be a null-treminated string,
-    // but fread just reads the file as binary, so we add it ourselves
-
-    file_buffer[bytes_read] = '\0';
-    for(int i = 0; i < sizeof(file_buffer); i++){
-      printf("%c", file_buffer[i]);
-    }
-
-    int key = 2;
-    int lines = 3;
-
-    unsigned char let_key;
-    int len = strlen(argv[key]);
-    char keyword[len];
-    for(int i = 0; i < len; i++){
-      keyword[i] = argv[key][i];
-      printf("%c", keyword[i]);
-    }
-
-    //Main code/////////////////////////////////////////////////////////////////
-
-    ptr = strstr(line, argv[key]);
-
-
     return 0;
 }
