aa95996394ed89e9223fb92a836ba983ac9821c4
abusa <abusa@umich.edu>
2019-09-26 18:12:59 -0400

    Replace substring.c

--- class4/substring.c
+++ class4/substring.c
@@ -1,69 +1,78 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 
 char file_buffer[64 * 1024];
-
-char *print_context(char *p, int count, int key_len) {
-    if (count == 0) {
-        int m = 0;
-        while (m < key_len) {
-            printf("%c", p[m]);
-            m++;
-        }
-        printf("\n");
-        printf("\n");
-        return &p[m];
-    }
-    int i = 0;
-    for (int n = 0; n < count; n++) {
-        while (p[-i] != '\n' && &p[-i] >= file_buffer) {
-            i++;
-        }
-        if (&p[-i] >= file_buffer) {
-            i++;
-        }
-    }
-    int j;
-    if (&p[-i] <= file_buffer) {
-        j = -i + 1;
-    } else {
-        j = -i + 2;
-    }
-    while (j < key_len) {
-        printf("%c", p[j]);
-        j++;
-    }
-    printf("\n");
-    printf("\n");
-    return &p[j];
-}
+char *get_context(char *context_p, int counter, int len); //func declaration
 
 int main(int argc, char **argv) {
-    if (argc != 4) {
+    if (argc != 4) { //No of arguments error
         fprintf(stderr, "usage: %s <file> <key> <lines before>\n", argv[0]);
         return 1;
     }
-    FILE *f = fopen(argv[1], "r");
+    FILE *f = fopen(argv[1], "r"); //open and read file
+
+    int len = strlen(argv[2]);
     int text = atoi(argv[3]);
-    int key_len = strlen(argv[2]);
-    if (!f) {
+
+    if (!f) { //file open error
         fprintf(stderr, "Could not open %s: ", argv[1]);
         perror("");
         return 1;
     }
-    fread(file_buffer, 1, sizeof(file_buffer) - 1, f);
-    if (!feof(f)) {
+
+    fread(file_buffer, 1, sizeof(file_buffer) - 1, f); //read file
+
+    if (!feof(f)) { //file read error
         fprintf(stderr, "Could not read entire file. Is it too big?\n");
         return 1;
     }
-    char *p = strstr(file_buffer, argv[2]);
-    while (p) {
-        char *p = print_context(p, text, key_len);
-        p = strstr(p, argv[2]);
+
+    char *context_p = strstr(file_buffer, argv[2]); //string comparison for key word and text
+
+    while (context_p) {
+        char *p = get_context(context_p, text, len);
+        context_p = strstr(p, argv[2]);
     }
+
     fclose(f);
     return 0;
+}
+
+//function
+char *get_context(char *context_p, int counter, int len) {
+    if (counter == 0) {
+        int m = 0;
+        while (m < len) {
+            printf("%c", context_p[m]);
+            m++;
+        }
+        printf("\n");
+        printf("\n");
+        return &context_p[m];
+    }
+
+    int i = 0;
+    for (int n = 0; n < counter; n++) {
+        while (context_p[-i] != '\n' && &context_p[-i] >= file_buffer) {
+            i++;
+        }
+        if (&context_p[-i] >= file_buffer) {
+            i++;
+        }
+    }
+    int j = 0;
+    if (&context_p[-i] <= file_buffer) {
+        j = -i + 1;
+    } else {
+        j = -i + 2;
+    }
+    while (j < len) {
+        printf("%c", context_p[j]);
+        j++;
+    }
+    printf("\n");
+    printf("\n");
+    return &context_p[j];
 }
