8c61b99ef16a7bcc74e41f84c8159aca6e76794c
Anviksha R Busa <abusa@beyster1620p04.engin.umich.edu>
2019-10-01 16:16:36 -0400

    bisection files 2

--- class8/bisection.c
+++ class8/bisection.c
@@ -8,32 +8,32 @@ double polynomial(double x)  {
 }
 
 void search(double x0, double x1)  {
-  if (polynomial(x0) * polynomial(x1) >= 0) {
-      printf("Wrong input bounds\n");
-  }
-  double xmid = x0;
-  double threshold = 1e-6;
-  while ((x1 - x0) > threshold) {
-      xmid = (x0 + x1)/2;
-      if (polynomial(xmid) == 0.0) {
-          break;
-      } else if (polynomial(xmid) * polynomial(x0) < 0){// Decide the side to repeat the steps
-          x1 = xmid;
-      } else {
-          x0 = xmid;
-      }
-      double ymid = polynomial(xmid);
-      printf("x_mid: %11.4e y_mid: %11.4e\n",xmid,ymid);
-  }
+    if (polynomial(x0) * polynomial(x1) >= 0) {
+        printf("Wrong input bounds\n");
+    }
+    double xmid = x0;
+    double threshold = 1e-6;
+    while ((x1 - x0) > threshold) {
+          xmid = (x0 + x1)/2;
+          if (polynomial(xmid) == 0.0) {
+              break;
+          } else if (polynomial(xmid) * polynomial(x0) < 0){// Decide the side to repeat the steps
+              x1 = xmid;
+          } else {
+              x0 = xmid;
+          }
+          double ymid = polynomial(xmid);
+          printf("x_mid: %11.4e y_mid: %11.4e\n", xmid, ymid);
+    }
 }
 
 int main(int argc, char **argv) {
-  if (argc != 3) { // Print error message if too many arguments
-      printf("Wrong num of arguments\n");
-      return 1;
-  }
-  double lower = atof(argv[1]);
-  double higher = atof(argv[2]);
-  search(lower, higher);
-  return 0;
+    if (argc != 3) { // Print error message if too many arguments
+        printf("Wrong num of arguments\n");
+        return 1;
+    }
+    double lower = atof(argv[1]);
+    double higher = atof(argv[2]);
+    search(lower, higher);
+    return 0;
 }
