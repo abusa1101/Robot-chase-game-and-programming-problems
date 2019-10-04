2331edbbdbc612b6f3e082cc8e22827ff7ec4bae
abusa <abusa@umich.edu>
2019-10-04 18:35:39 -0400

    ras

--- homework2/rasterize.c
+++ homework2/rasterize.c
@@ -21,16 +21,26 @@ int main(int argc, char **argv) {
   bmp.data = calloc(bmp.width * bmp.height, sizeof(color_bgr_t));
   int color_value = 255;
   color_bgr_t color = {color_value, color_value, color_value};
-  //int input = 2;
+  pg_vector_t rect_vec = {0};
+  pg_create(&rect_vec);
 
   if (input == 2) {
      bresenham(10,10,200,200, &bmp, color);
+   } else if (input == 3) {
+      double xc = 0.0;
+       double yc = 0.0;
+
+       give_rect(&rect_vec, 4.0, 4.0, xc, yc);
+       cd2pixel(&rect_vec);
+       pg_draw(&bmp, color, &rect_vec);
+   } else if (input == 4) {
+     double xc = 2.0;
+       double yc = 2.0;
+       give_rect(&rect_vec, 4.0, 4.0, xc, yc);
+       cd2pixel(&rect_vec);
+       pg_draw(&bmp, color, &rect_vec);
    }
-  //else if (input == 3) {
-  //   test3(&bmp, color);
-  // } else if (input == 4) {
-  //   test4(&bmp, color);
-  // } else if (input == 5) {
+   //else if (input == 5) {
   //   test5(&bmp, color);
   // } else if (input == 6) {
   //   test6(&bmp, color);
@@ -93,7 +103,7 @@ int main(int argc, char **argv) {
   sleep(1);
   // pg_free(&transformed_vec);
   //pg_free(&tri_vec);
-  //pg_free(&rect_vec);
+  pg_free(&rect_vec);
   free(serialized_bmp);
   free(bmp.data);
   return 0;
