1ed26de2580d9340071fbb60f35e26fb2f35d26f
abusa <abusa@umich.edu>
2019-10-05 17:05:20 -0400

    ras

--- homework2/test.c
+++ homework2/test.c
@@ -50,7 +50,7 @@ void CallFunctions(bitmap_t bmp, color_bgr_t color, int input,
         give_tri(&rect_vec, 21.0, 28.0, 400, 400);
         rotate(&rect_vec, &transformed_vec, -30);
         cd2pixel(&transformed_vec);
-        //tri_draw(&bmp, color, &transformed_vec);
+        tri_draw(&bmp, color, &transformed_vec);
         callb2(&fillbuff, &transformed_vec);
         tri_fill(&bmp, color, &transformed_vec, &fillbuff);
     }
--- homework2/vectors.h
+++ homework2/vectors.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "bmp.h"
 
 typedef struct pg_points {
     double x;
