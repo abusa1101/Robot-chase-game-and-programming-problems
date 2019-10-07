e9490cd9946adf51092dd7a7e34c02e242a51ddd
abusa <abusa@umich.edu>
2019-10-07 02:57:35 -0400

    bbf

--- homework2/movement.h
+++ homework2/movement.h
@@ -1,5 +1,3 @@
-//#ifndef MOVEMENT_H
-//#define MOVEMENT_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,79 +16,6 @@ typedef struct pg_vector {
     int capacity;
     int size;
 } pg_vector_t;
-// typedef struct pos_data {
-//     double x;
-//     double y;
-// } pos_data_t;
-//
-// typedef struct rob_position {
-//     pos_data_t *data;
-//     int capacity;
-//     int size;
-// } rob_position_t;
-//
-// typedef struct unitvec {
-//     double x;
-//     double y;
-// } unitvec_t;
-//
-// typedef struct lamp {
-//     rob_position_t location;
-//     color_bgr_t color;
-//     double size;
-//     double rot;
-//     double power;
-// } lamp_t;
-//
-// typedef struct compare {
-//     double l1;
-//     double l2;
-//     double l3;
-// } compare_t;
-//
-// typedef struct robot {
-//   rob_position_t location;
-//   color_bgr_t color;
-//   unitvec_t dir;
-//   unitvec_t eye_l;
-//   unitvec_t eye_r;
-//   double dist_sq;
-//   double move_l;
-//   double move_r;
-//   double theta;
-//   double fwd_dist;
-//   double max_movement;
-//   double wheel_base;
-//   double width;
-//   double length;
-// } robot_t;
-//
-// typedef struct game {
-//   color_bgr_t color;
-//   robot_t robot;
-//   lamp_t lamp;
-//   double n_lamp;
-// } game_t;
-
-////////////////////////////////////////////FUNCTIONS/////////////////////////////////////////////////////////////
-//Movement
-//void setConstants(robot_t robot, lamp_t lamp1, lamp_t lamp2, lamp_t lamp3);
-// // double dotP(game_t game);
-// // void rotateP(double dir, double angle, double x_pivot, double y_pivot);
-// // void activateMove(game_t game);
-//
-//Graphics
- //void initGraphics(game_t game, pg_vector_t *rect_vec);
-// // void resetGraphics (game_t game, pg_points_t *rect_vec);
-// // void updateGraphics(game_t game, pg_points_t *rect_vec);
-//
-// //Collision
-// // int line_intersection(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3);
-// // int pg_intersection(polygon_t *pg1, polygon_t *pg2);
-// // int pg_collision(polygon_t *pg1, polygon_t *pg2);
-// // int check4containment(polygon_t *pg, double x, double y);
-//
-
 
 void pg_create(pg_vector_t *v);
 
@@ -125,6 +50,3 @@ void tri_fill(bitmap_t *bmp, color_bgr_t color, pg_vector_t *tri_vec, pg_vector_
 void rotate(pg_vector_t *rect_vec, pg_vector_t *transformed_vec, double angle);
 
 void rotate2(pg_vector_t *rect_vec, pg_vector_t *transformed_vec, double angle, double x_pivot, double y_pivot);
-
-
-//#endif
