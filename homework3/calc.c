b797bcc5429617f68b6aeaafbf8b6d62b1e14ae1
Anviksha R Busa <abusa@beyster1695p14.engin.umich.edu>
2019-10-18 16:05:43 -0400

    calc final file?

--- homework3/calc.c
+++ homework3/calc.c
@@ -5,16 +5,6 @@
 #include <math.h>
 #include <ctype.h>
 
-//Functions List
-void skip_whitespace(char **str);
-char peek(char **str);
-char peek_num(char **str);
-char parse_token(char **str);
-double digit(char **str);
-double give_digits(char **str);
-double number_literal(char **str);
-double parenthetical(char **str);
-double multiplicative(char **str);
 double additive(char **str);
 
 int main(int argc, char **argv) {
@@ -55,6 +45,16 @@ char parse_token(char **str) {
     return token;
 }
 
+char parse_token_num(char **str) {
+    char token = *str[0];
+    if (!token) {
+        fprintf(stderr, "Parsing Error: Unexpected end of string\n");
+        exit(1);
+    }
+    (*str)++;
+    return token;
+}
+
 double digit(char **str) {
     char token = *str[0];
     double token_value = 0;
@@ -71,54 +71,56 @@ double digit(char **str) {
 double give_digits(char **str) {
     double value_left = 0;
     double value_right = 0;
-    while (isdigit(peek(str) - '0') == 0) { //peek or peek_num? skip white spaces?
+    bool digits_exist = false;
+    while (isdigit(peek_num(str))) { //peek or peek_num? skip white spaces?
         printf("in give_digits while loop\n");
-        if (peek(str) < '0' || peek(str) > '9') {
-            printf("in break loop\n");
-            break;
-        }
         value_left = value_left * 10 + digit(str);
         (*str)++; //moves pointer to next digit
+        digits_exist = true;
     } //exits loop if no digits- i.e., it should hit a decimal point
 
-    if (peek(str) == '.') {
-        char throwaway = parse_token(str);
+    if (peek_num(str) == '.') {
+        parse_token(str);
         double divisor = 0.1;
-        while (isdigit(peek(str))) {
+        while (isdigit(peek_num(str))) {
             value_right = value_right + digit(str) * divisor;
             divisor /= 10.0;
+            (*str)++;
+            digits_exist = true;
         }
+    }
+    if (!digits_exist) {
+        fprintf(stderr, "Error: Expected digits, got '%c' instead\n", parse_token_num(str));
+        exit(1);
     }
     return value_left + value_right;
 }
 
 double number_literal(char **str) {
-    if (peek_num(str) == ' ') {
-        skip_whitespace(str);
-    }
+    skip_whitespace(str);
     int sign = 1; //assume positive value default
     if (peek_num(str) == '+' || peek_num(str) == '-') {
         if (peek_num(str) == '-') {
             sign = -1;
-            char throwaway = parse_token(str); //move pointer and detect abrupt end
         }
+        parse_token(str); //move pointer and detect abrupt end
     }
-    double value = 0;
-    if(isdigit(peek(str) - '0') == 0) {
-        value = give_digits(str);
-    }
+    double value = give_digits(str);
+
     return sign * value;
 }
 
 double parenthetical(char **str) {
     double value = 0;
     if (peek(str) == '(') {
-        char token = parse_token(str); //or can i just do skip_whitespace(str) and (*str)++?
+        parse_token(str); //or can i just do skip_whitespace(str) and (*str)++?
         value = additive(str); //is this = or += because multiple operations???
-        if (parse_token(str) != ')') {
-            fprintf(stderr, "Parsing Error: Expected ')' but received '%c' instead.\n", parse_token(str));
+        char error_char = parse_token(str);
+        if (error_char != ')') {
+            fprintf(stderr, "Parsing Error: Expected ')' but received '%c' instead.\n", error_char);
             exit(1);
         }
+        
     } else {
         value = number_literal(str);
     }
@@ -129,8 +131,10 @@ double multiplicative(char **str) {
     double value = parenthetical(str);
     while (peek(str) == '*' || peek(str) == '/') {
         if (peek(str) == '*') {
+            (*str)++;
             value *= parenthetical(str);
         } else {
+            (*str)++;
             value /= parenthetical(str);
         }
     }
@@ -139,10 +143,13 @@ double multiplicative(char **str) {
 
 double additive(char **str) {
     double value = multiplicative(str);
+
     while (peek(str) == '+' || peek(str) == '-') {
         if (peek(str) == '+') {
+            (*str)++;
             value += multiplicative(str);
         } else {
+            (*str)++;
             value -= multiplicative(str);
         }
     }
