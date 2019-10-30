5296f8c7c48307e0c6bf2fea5c18b256443af47c
Anviksha R Busa <abusa@beyster1620p21.engin.umich.edu>
2019-10-30 17:19:05 -0400

    chase f

--- homework3/chase_functions.c
+++ homework3/chase_functions.c
@@ -25,7 +25,7 @@
 #define MAP_H (HEIGHT / BLOCK_SIZE)
 #define MAX_VEL 12
 #define ROB_W 20
-#define ROB_L 26.67
+#define ROB_L (ROB_W * 4 / 3)
 #define MAX_DEPTH 3
 
 // Vector/low-level Operations
