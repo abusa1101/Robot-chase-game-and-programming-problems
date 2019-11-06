f78020d8a4a21d1e811e18657df32ab9716d4dae
Anviksha Busa <abusa@umich.edu>
2019-11-06 00:00:53 -0500

    htf

--- homework4/hashtable.h
+++ homework4/hashtable.h
@@ -6,5 +6,9 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <ctype.h>
+#define TABLE_SIZE 128
 
-typedef struct hashtable hashtable_t;
+// typedef struct hashtable hashtable_t;
+
+void read_word(FILE *fp, char *word, int char_num);
