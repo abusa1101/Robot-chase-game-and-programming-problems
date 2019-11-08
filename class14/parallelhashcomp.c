7096d001b914b11c5e3a7ae71933c597af4262f3
Anviksha R Busa <abusa@eecs2420p05.engin.umich.edu>
2019-11-08 17:40:38 -0500

    f

--- class14/parallelhashcomp.c
+++ class14/parallelhashcomp.c
@@ -5,15 +5,26 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 #include <pthread.h>
-
 #define TABLE_SIZE 8192
-#define N_THREADS 16
-#define DEBUG_ON 0
-
-const uint32_t factor32 = 2654435769;
-
 uint32_t table_hash_keys[256];
+
+typedef struct test_entry {
+    uint8_t *data;
+    int n;
+} test_entry_t;
+
+typedef struct thread_info {
+    int num;
+    uint32_t (*hash_f)(uint8_t *key, int value);
+    uint32_t (*reduce_f)(uint32_t hash);
+    float coll_time;
+    int collisions;
+    int n_entries;
+    test_entry_t *entries;
+    pthread_t thread;
+} thread_info_t;
 
 double seconds_now(void) {
     struct timespec now;
@@ -36,9 +47,6 @@ uint32_t add_hash(uint8_t *data, int n) {
 void setup_table_hash(void) {
     for (int i = 0; i < 256; i++) {
         table_hash_keys[i] = rand();
-        if (DEBUG_ON) {
-            printf("hash key: %d\n", table_hash_keys[i]);
-        }
     }
 }
 
@@ -70,10 +78,12 @@ uint32_t djb2a_hash(uint8_t *data, int n) {
 }
 
 uint32_t fnv1a_hash(uint8_t *data, int n) {
-    uint32_t hash = 2166136261;
-    uint32_t prime = 16777619;
+    uint32_t FNV_offset_basis = 2166136261;
+    uint32_t FNV_prime = 16777619;
+    uint32_t hash = FNV_offset_basis;
     for (int i = 0; i < n; i++) {
-        hash = (hash ^ *data) * prime;
+        hash ^= *data;
+        hash *= FNV_prime;
         data++;
     }
     return hash;
@@ -85,86 +95,74 @@ uint32_t rotate_left(uint32_t value, uint32_t count) {
 
 uint32_t fxhash32_step(uint32_t hash, uint32_t value) {
     const uint32_t key = 0x27220a95;
+    //const uint64_t key = 0x517cc1b727220a95;
     return (rotate_left(hash, 5) ^ value) * key;
 }
 
 uint32_t fxhash32_hash(uint8_t *data, int n) {
     uint32_t hash = 0;
-    while (n > 3) {
+    int data_chunks = n / 4;
+    for (int i = 0; i < data_chunks; i++) {
         uint32_t number;
         memcpy(&number, data, sizeof(number));
-        hash = fxhash32_step(hash, number);
-        n -= 4;
         data += 4;
+        hash = fxhash32_step(hash, number);
     }
-    while (n > 0) {
+    int remaining_bytes = n - (4 * data_chunks);
+    for (int i = 0; i < remaining_bytes; i++) {
         hash = fxhash32_step(hash, *data);
-        n--;
-        data++;
+        data += 1;
     }
     return hash;
 }
 
 uint32_t modulo2_reduce(uint32_t hash) {
-    return hash & ((1 << 13) - 1);
+    hash = hash & ((1 << 13) - 1);
+    return hash;
 }
 
 uint32_t modulo_prime_reduce(uint32_t hash) {
-    return hash % 8191;
+    hash = hash % 8191;
+    return hash;
 }
 
 uint32_t fibonacci32_reduce(uint32_t hash) {
-    return (hash * factor32) >> (32 - 13);
+    const uint32_t factor32 = 2654435769;
+    hash = (uint32_t)(hash * factor32) >> (32 - 13);
+    return hash;
 }
 
-typedef struct test_entry {
-    uint8_t *data;
-    int n;
-} test_entry_t;
 void evaluate_hash_reduce(int n_entries, test_entry_t *entries,
                           uint32_t (*hash_f)(uint8_t *, int), uint32_t (*reduce_f)(uint32_t),
-                          float *time, int *coll) {
+                          float *timetaken, int *coll_count) {
     double start = seconds_now();
-    long iterations = 0;
+    long loop_num = 0;
     while (seconds_now() - start < 0.5) {
         for (int i = 0; i < n_entries; i++) {
             reduce_f(hash_f(entries[i].data, entries[i].n));
-            iterations++;
+            loop_num++;
         }
     }
-    bool is_hashed[8192] = {0};
+    bool table_arr[8192] = {0};
     int collision;
     collision = 0;
     for (int i = 0; i < n_entries; i++) {
         uint32_t hash = reduce_f(hash_f(entries[i].data, entries[i].n));
-        if (is_hashed[hash]) {
+        if (table_arr[hash]) {
             collision++;
         } else {
-            is_hashed[hash] = true;
+            table_arr[hash] = true;
         }
     }
-    *coll = collision;
-    double avg_time = ((seconds_now() - start) / (iterations)) * 1000000000;
-    *time = avg_time;
-    // double elapsed = (clock() - start) / (double)CLOCKS_PER_SEC;
-    // printf("complex_calculation() took %.12f seconds\n", elapsed);
+    *coll_count = collision;
+    double coll_time = ((seconds_now() - start) / (loop_num)) * pow(10, 9);
+    *timetaken = coll_time;
 }
 
-typedef struct thread_info {
-    int num;
-    int n_entries;
-    test_entry_t *entries;
-    uint32_t (*hash_f)(uint8_t *key, int value);
-    uint32_t (*reduce_f)(uint32_t hash);
-    float avg_time;
-    int collisions;
-    pthread_t thread;
-} thread_info_t;
-
 void *thread_start(void *user) {
-    thread_info_t *info = user; //casting null pointer to the correct type.
+    thread_info_t *info = user;
     evaluate_hash_reduce(info->n_entries, info->entries, info->hash_f,
-                         info->reduce_f, &info->avg_time, &info->collisions);
+                         info->reduce_f, &info->coll_time, &info->collisions);
     return NULL;
 }
 
@@ -176,21 +174,17 @@ int main(int argc, char **argv) {
     int n_entries = max_entries;
     test_entry_t *entries = calloc(max_entries, sizeof(test_entry_t));
 
-    for (int i = 0; i < 1000; i++) {
-        uint16_t num = i;
-        entries[i].data = malloc(sizeof(num));
-        memcpy(entries[i].data, &num, sizeof(num));
-        entries[i].n = sizeof(num);
+    for (uint16_t i = 0; i < 1000; i++) {
+        entries[i].data = malloc(sizeof(i));
+        memcpy(entries[i].data, &i, sizeof(i));
+        entries[i].n = sizeof(i);
     }
-    FILE *book = fopen("book.txt", "r");
-    if (!book) {
-        fprintf(stderr, "file not found!\n");
-        return 1;
-    }
-    for (int i = 1000; i < n_entries; i++) {
-        entries[i].data = malloc(sizeof(char) * 100);
-        char *line = fgets((char *)entries[i].data, 100, book);
-        entries[i].n = (int)strlen(line);
+    FILE *fp = fopen("book.txt", "r");
+    char buffer[256];
+    for (int i = 1000; i < max_entries; i++) {
+        char *line = fgets(buffer, 256, fp);
+        entries[i].data = (uint8_t *)strdup(line);
+        entries[i].n = strlen(line);
     }
 
     uint32_t (*hash_functions[])(uint8_t *, int) = {add_hash, table_a_hash, table_b_hash,
@@ -222,16 +216,18 @@ int main(int argc, char **argv) {
                 }
                 for (int j = 0; j < limit; j++) {
                     pthread_join(thread_infos[j].thread, NULL);
-                    printf("%.2fns per iteration, with %d collisions\n", thread_infos[j].avg_time,
+                    printf("%.2fns per iteration, with %d collisions\n", thread_infos[j].coll_time,
                            thread_infos[j].collisions);
                 }
             }
         }
     }
 
-    for (int i = 0; i < n_entries; i++) {
+    for (int i = 0; i < max_entries; i++) {
         free(entries[i].data);
+        entries[i].data = NULL;
     }
     free(entries);
+    entries = NULL;
     return 0;
 }
