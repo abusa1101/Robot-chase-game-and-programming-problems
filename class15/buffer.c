965327f4dc2b799b1a59113121ede8ac12f3d579
Anviksha R Busa <abusa@beyster1620p21.engin.umich.edu>
2019-11-12 17:10:04 -0500

    f

--- class15/buffer.c
+++ class15/buffer.c
@@ -16,26 +16,27 @@ bool producer_finished = false;
 bool mutex_mode = false;
 
 void *producer_thread(void *user) {
-    // printf("here in PT\n");
-    for (int i = 0; i < MAX_MSG; i++) { //do we do this????
-        //printf("BS,MAX_MSG: %d, %d,\n", buffer_size, MAX_MSG);
-        if (mutex_mode) {
-            pthread_mutex_lock(&mutex);
-        }
-        buffer[i % BUFFER_CAPACITY] = i;
-        buffer_size++;
-        if (mutex_mode) {
-            pthread_mutex_unlock(&mutex);
-        }
-        if (buffer_size == BUFFER_CAPACITY) {
-            // printf("here in if size equal\n");
-            //wait a nanosecond, or as little as possible
-            struct timespec interval = {0, 1}; // former number-seconds, latter-nanoseconds
-            nanosleep(&interval, NULL);
+    int msg = 0;
+    while (msg < MAX_MSG) { //do we do this????
+        if (buffer_size < BUFFER_CAPACITY) {
+            if (mutex_mode) {
+                pthread_mutex_lock(&mutex);
+                buffer[buffer_size] = msg;
+                buffer_size++;
+                pthread_mutex_unlock(&mutex);
+            } else {
+                buffer[buffer_size] = msg;
+                buffer_size++;
+            }
+            msg ++;
+            while (buffer_size >= BUFFER_CAPACITY) {
+                //wait a nanosecond, or as little as possible
+                struct timespec interval = {0, 1}; //former number-seconds, latter-nanoseconds
+                nanosleep(&interval, NULL);
+            }
         }
     }
     producer_finished = true;
-    // printf("here PF is true now\n");
     return NULL;
 }
 
@@ -45,12 +46,9 @@ int main(int argc, char **argv) {
             mutex_mode = true;
         }
     }
-
-    // printf("here in main\n");
     //Start producer thread
     pthread_t thread;
     pthread_create(&thread, NULL, producer_thread, NULL);
-    // printf("here after create\n");
     //Consumer thread
     while (1) {
         if (buffer_size > 0) {
@@ -58,18 +56,17 @@ int main(int argc, char **argv) {
                 pthread_mutex_lock(&mutex);
             }
             buffer_size --;
+            //printf("BS: %d\n", buffer_size);
             printf("%d\n", buffer[buffer_size]);
             if (mutex_mode) {
                 pthread_mutex_unlock(&mutex);
             }
             if (buffer_size == 0 && producer_finished) {
-                // printf("here in break while\n");
                 break;
             }
         }
 
     }
-    // printf("here broke out of while\n");
     //pthread_join(thread_infos[i].thread, NULL);
     return 0;
 }
