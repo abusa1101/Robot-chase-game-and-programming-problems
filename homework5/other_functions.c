2c05835fe3b9e7c500e5e5edea3a7038873a0057
Anviksha R Busa <abusa@eecs2420p12.engin.umich.edu>
2019-12-02 14:10:23 -0500

    f

--- homework5/other_functions.c
+++ homework5/other_functions.c
@@ -92,8 +92,8 @@ void serving_img(bitmap_t bmp, state_t *state) {
 //Movement
 void init_values(state_t *state) {
     state->chaser.theta = 0;
-    state->chaser.x = WIDTH / 2;
-    state->chaser.y = HEIGHT / 2;
+    state->chaser.x = (double)WIDTH / 2;
+    state->chaser.y = (double)HEIGHT / 2;
     //state->runner.theta = 0;
     state->initial_runner_idx = 17;
     give_runner_pos(state, state->initial_runner_idx);
@@ -136,7 +136,7 @@ void chaser_moves(state_t *state) {
 
 //Potential Field
 void potential_field_control(state_t *state) {
-    double robot_r = sqrt(pow((ROB_W / 2), 2) + pow((ROB_L / 2), 2));
+    double robot_r = sqrt(pow((double)(ROB_W / 2), 2) + pow((double)(ROB_L / 2), 2));
     double wall_r = BLOCK_SIZE / sqrt(2);
     double fx = 0.0; //forces on robot
     double fy = 0.0;
@@ -239,11 +239,11 @@ void update_parameters(state_t *state, bool action_is_up) {
 void reset_simulation(state_t *state) {
     srand(0);
     state->chaser.theta = 0;
-    state->chaser.x = WIDTH / 2;
-    state->chaser.y = HEIGHT / 2;
+    state->chaser.x = (double)WIDTH / 2;
+    state->chaser.y = (double)HEIGHT / 2;
     state->timestep = 0;
-    state->runner.x = ((double)(state->initial_runner_idx % MAP_W) + 0.5) * BLOCK_SIZE;
-    state->runner.y = ((double)(state->initial_runner_idx / MAP_W) + 0.5) * BLOCK_SIZE;
+    state->runner.x = (double)((state->initial_runner_idx % MAP_W) + 0.5) * BLOCK_SIZE;
+    state->runner.y = (double)((state->initial_runner_idx / MAP_W) + 0.5) * BLOCK_SIZE;
     state->runner.fwd_vel = 0;
     state->runner.ang_vel = 0;
     state->runner.theta = 0;
