68e84f5fcde197f5cc0365a244b178240f94b429
Anviksha Busa <abusa@umich.edu>
2019-12-11 12:15:52 -0500

    f

--- homework6/world.c
+++ homework6/world.c
@@ -53,13 +53,13 @@ void print_interface(state_t *state) {
 
 void robot_init(state_t *state) {
     state->chaser.theta = 0;
-    state->runner.theta = 0;
+    //state->runner.theta = 0;
     state->chaser.is_runner = false;
     state->runner.is_runner = true;
     state->chaser.x = (double)WIDTH / 2;
     state->chaser.y = (double)HEIGHT / 2;
     state->settings_message.initial_runner_idx = 17;
-    //give_runner_pos(state, state->settings_message.initial_runner_idx);
+    give_runner_pos(state, state->settings_message.initial_runner_idx);
     state->settings_message.delay_every = 1;
     state->settings_message.to_goal_magnitude = 50.0;
     state->settings_message.to_goal_power = 0;
